4da93829213112427fc91bcba034dd482232820b
Gautam Shah <gautamshah317@gmail.com>
2025-06-06 15:02:09 +0530

    working on chat container

--- Client/src/components/ChatContainer.jsx
+++ Client/src/components/ChatContainer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import assets from "../assets/assets";
+import assets, { imagesDummyData, messagesDummyData } from "../assets/assets";
 
-const ChatContainer = ({selectedUser,setSelectedUser}) => {
+const ChatContainer = ({ selectedUser, setSelectedUser }) => {
   return selectedUser ? (
     <div className="h-full overflow-scroll relative backdrop-blur-lg">
+      {/* --------------header-------------------- */}
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
         <img
           src={assets.profile_martin}
@@ -14,8 +15,59 @@ const ChatContainer = ({selectedUser,setSelectedUser}) => {
           Gautam Shah
           <span className="w-2 h-2 rounded-full bg-green-500"></span>
         </p>
-        <img onClick={()=>setSelectedUser(null)} src={assets.arrow_icon} alt="arrow-icon" className="md:hidden max-w-7" />
-        <img src={assets.help_icon} className="max-md:hidden max-w-5" alt="help-icon" />
+        <img
+          onClick={() => setSelectedUser(null)}
+          src={assets.arrow_icon}
+          alt="arrow-icon"
+          className="md:hidden max-w-7"
+        />
+        <img
+          src={assets.help_icon}
+          className="max-md:hidden max-w-5"
+          alt="help-icon"
+        />
+      </div>
+
+      {/* -----------------chat area--------------- */}
+      <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
+        {messagesDummyData.map((msg, index) => (
+          <div
+            key={index}
+            className={`flex items-end gap-2 justify-end ${
+              msg.senderId !== "680f50e4f10f3cd28382ecf9" && "flex-row-reverse"
+            }`}
+          >
+            {msg.image ? (
+              <img
+                src={msg.image}
+                alt="image"
+                className="max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8"
+              />
+            ) : (
+              <p
+                className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${
+                  msg.senderId === "680f50e4f10f3cd28382ecf9"
+                    ? "rounded-br-none"
+                    : "rounded-bl-none"
+                }`}
+              >
+                {msg.text}
+              </p>
+            )}
+            <div className="text-center text-xs">
+              <img
+                src={
+                  msg.senderId === "680f50e4f10f3cd28382ecf9"
+                    ? assets.avatar_icon
+                    : assets.profile_martin
+                }
+                alt="profile-img"
+                className="w-7 rounded-full"
+              />
+              <p className="text-gray-500">{msg.createdAt}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   ) : (
@@ -23,7 +75,7 @@ const ChatContainer = ({selectedUser,setSelectedUser}) => {
       <img src={assets.logo_icon} alt="logo" className="max-w-16" />
       <p className="text-lg font-medium text-white">Chat Anytime & Anywhere</p>
     </div>
-  )
+  );
 };
 
 export default ChatContainer;
