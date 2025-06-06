1371010aabe796295e3b2d1fc942f75648cb143a
Gautam Shah <gautamshah317@gmail.com>
2025-06-06 14:44:36 +0530

    working on chat container component

--- Client/src/components/ChatContainer.jsx
+++ Client/src/components/ChatContainer.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import assets from "../assets/assets";
 
-const ChatContainer = () => {
-  return (
-    <div>
-      
+const ChatContainer = ({selectedUser,setSelectedUser}) => {
+  return selectedUser ? (
+    <div className="h-full overflow-scroll relative backdrop-blur-lg">
+      <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
+        <img
+          src={assets.profile_martin}
+          alt="profile-img"
+          className="w-8 rounded-full"
+        />
+        <p className="flex-1 text-lg text-white flex items-center gap-2">
+          Gautam Shah
+          <span className="w-2 h-2 rounded-full bg-green-500"></span>
+        </p>
+        <img onClick={()=>setSelectedUser(null)} src={assets.arrow_icon} alt="arrow-icon" className="md:hidden max-w-7" />
+        <img src={assets.help_icon} className="max-md:hidden max-w-5" alt="help-icon" />
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden">
+      <img src={assets.logo_icon} alt="logo" className="max-w-16" />
+      <p className="text-lg font-medium text-white">Chat Anytime & Anywhere</p>
     </div>
   )
-}
+};
 
-export default ChatContainer
+export default ChatContainer;
