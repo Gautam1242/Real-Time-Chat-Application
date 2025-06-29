c16819e66f85d1789f4cc290edf33cf4efa11938
Gautam Shah <gautamshah317@gmail.com>
2025-06-29 20:49:42 +0530

    working on login page

--- Client/src/pages/LoginPage.jsx
+++ Client/src/pages/LoginPage.jsx
@@ -9,14 +9,25 @@ const LoginPage = () => {
   const [bio,setBio]=useState("");
   const [isDataSubmitted,setIsDataSubmitted]=useState(false);
 
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+    if(currentState==='Sign up' && !isDataSubmitted){
+      setIsDataSubmitted(true);
+      return;
+    }
+  }
+
   return (
     <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
       {/* Left */}
       <img src={assets.logo_big} alt="logo" className="w-[min(30vw,250px)]" />
       {/* right */}
-      <form className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg">
+      <form onSubmit={handleSubmit} className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg">
       <h2 className="font-medium text-2xl flex justify-between items-center">{currentState}
-        <img src={assets.arrow_icon} alt="" className="w-5 cursor-pointer"/>
+        {
+          isDataSubmitted && <img onClick={()=>setIsDataSubmitted(false)} src={assets.arrow_icon} alt="" className="w-5 cursor-pointer"/>
+        }
       </h2>
       {currentState==="Sign Up" && !isDataSubmitted && (
         <input onChange={(e)=>setFullName(e.target.value)} value={fullName} type="text" className="p-2 border border-gray-500 rounded-md focus:outline-none" placeholder="Full Name" required/>
