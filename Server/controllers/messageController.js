02442c904568b8c739f4c01caea9022f380f6a64
Gautam Shah <gautamshah317@gmail.com>
2025-07-23 18:57:01 +0530

    completed the backend now i will integrated the apis in frontend

--- Server/controllers/messageController.js
+++ Server/controllers/messageController.js
@@ -1,1 +1,123 @@
+
+import User from "../models/User.js"
+import Message from "../models/Message.js"
+import cloudinary from "../lib/cloudinary.js";
+import { io,userSocketMap } from "../server.js";
 // get all users except the logged in user 
+export const getUsersForSidebar=async(req,res)=>{
+    try {
+        const userId=req.user._id;
+        const filteredUsers=await User.find({_id:{$ne:userId}}).select("-password");
+
+        // count no of messages not seen 
+        const unseenMessages={}
+        const promises=filteredUsers.map(async(user)=>{
+            const messages=await Message.find({
+                senderId:user._id,
+                receiverId:userId,
+                seen:false
+            })
+
+            if(messages.length>0){
+                unseenMessages[user._id]=messages.length;
+            }
+        });
+        await Promise.all(promises);
+        res.status(200).json({
+            success:true,
+            users:filteredUsers,
+            unseenMessages
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+// Get all messages for selected user
+export const getMessages=async(req,res)=>{
+    try {
+        // selected user id
+        const {id:selectedUserId}=req.params;
+        // login user id
+        const myID=req.user._id;
+        // get all the messages between the two users
+        const messages=await Message.find({
+            $or:[
+                {senderId:myID,receiverId:selectedUserId},
+                {senderId:selectedUserId,receiverId:myID},
+            ]
+        });
+        // marking the seen property as read
+        await Message.updateMany({senderId:selectedUserId,receiverId:myID},{seen:true});
+
+        res.status(200).json({
+            success:true,
+            messages
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+// api to mark messages as seen using messages id
+export const markMessageAsSeen=async(req,res)=>{
+    try {
+        const {id}=req.params;
+        await Message.findByIdAndUpdate(id,{seen:true});
+        res.status(200).json({
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+// api to send message to selected user 
+export const sendMessage=async(req,res)=>{
+    try {
+        const {selectedUserId}=req.params.id;
+        const userId=req.user._id;
+        const {text,image}=req.body;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse=await cloudinary.uploader.upload(image);
+            imageUrl=uploadResponse.secure_url;
+        }
+        const newMessage=await Message.create({
+            senderId:userId,
+            receiverId:selectedUserId,
+            text,
+            image:imageUrl
+        });
+
+        // emit the new message to the receiver's socket
+        const receiverSocketId=userSocketMap[selectedUserId];
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage",newMessage);
+        }
+        res.status(200).json({
+            success:true,
+            message:newMessage
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
--- Server/server.js
+++ Server/server.js
@@ -4,27 +4,53 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import router from "./routes/userRoute.js";
+import messageRouter from "./routes/messageRoute.js";
+import { Server } from "socket.io";
 
 // create express app and http server
 const app = express();
 const server = http.createServer(app); // socket io supports this http server
 
+
+// initialize socket.io server
+export const io=new Server(server,{
+  cors:{origin:"*"}
+})
+
+// Store online users
+export const userSocketMap={}; // {userId:socketId}
+
+// Socket.io connection handler
+io.on("connection",(socket)=>{
+  const userId=socket.handshake.query.userId;
+  console.log("User Connected",userId);
+
+  if(userId) userSocketMap[userId]=socket.id;
+
+  // Emit online users to all connected clients
+  io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+  socket.on("disconnect",()=>{
+    console.log("User Disconnecetd",userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+  })
+})
+
 // middleware setup
 app.use(express.json({ limit: ["4mb"] }));
 app.use(cors());
-
 app.use("/api/status", (req, res) => {
   res.send("Server is live");
 });
 
-// connect to mongo db
-await connectDB();
-
-const PORT = process.env.PORT || 5000;
-
 // user end points
 app.use("/api/auth", router);
+app.use("/api/messages",messageRouter)
 
+// connect to mongo db
+await connectDB();
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log("Server is running on PORT : " + PORT);
 });
